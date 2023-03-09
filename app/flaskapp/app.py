8e89b35b74bac704fe18cd68d785d63f7fa4f8c0
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-10 07:52:23 +0900

    black フォーマッター

--- app/flaskapp/app.py
+++ app/flaskapp/app.py
@@ -1,33 +1,54 @@
 from flask import Flask, render_template, send_from_directory, jsonify, request
 from flask_cors import CORS
+from black import format_str, FileMode
+
 
 # frontend (vite+vue)のリソースを渡すのが役目
-app = Flask(__name__, static_folder='./frontend/dist/assets', template_folder='./frontend/dist')
+app = Flask(
+    __name__, static_folder="./frontend/dist/assets", template_folder="./frontend/dist"
+)
 
-CORS(app, origins=["http://localhost:5173"]) # CORS 対応
+CORS(app, origins=["http://localhost:5173"])  # CORS 対応
+
+
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    return response
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+# [black]自動フォーマット
+# @app.after_request
+# def after_request(response):
+#     if response.content_type.startswith("text/html"):
+#         response.set_data(
+#             format_str(response.get_data(as_text=True), mode=FileMode())
+#         )
+#     return response
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def index(path):
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/<path:filename>', methods=['GET'])
+
+@app.route("/<path:filename>", methods=["GET"])
 def resource(filename):
     return send_from_directory("../frontend/dist", filename)
 
-@app.route('/assets/<path:filename>', methods=['GET'])
+
+@app.route("/assets/<path:filename>", methods=["GET"])
 def assets_resource(filename):
     return send_from_directory("../frontend/dist/assets", filename)
 
+
 # WEB-API定義
-@app.route('/api/user', methods=['POST'])
+@app.route("/api/user", methods=["POST"])
 def get_user():
     return jsonify({"message": "aa43"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
