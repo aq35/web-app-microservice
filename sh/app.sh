124f2053a3ae0fd831dc800724f3017645d2612e
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-11 08:41:06 +0900

    Update app.sh

--- sh/app.sh
+++ sh/app.sh
@@ -10,10 +10,10 @@ read -p "(1,2)を選択してください。: " choice
 
 case $choice in
   1)
-    ./scripts/libs/docker-exec.sh $APP_CONTAINER_NAME python3 linter.py
+    ./sh/libs/docker-exec.sh $APP_CONTAINER_NAME python3 linter.py
     ;;
   2)
-    ./scripts/libs/docker-exec.sh $APP_CONTAINER_NAME ls
+    ./sh/libs/docker-exec.sh $APP_CONTAINER_NAME ls
     ;;
   *)
     echo "Invalid choice"
