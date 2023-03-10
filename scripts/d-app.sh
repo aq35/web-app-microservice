1752ef6625dcfdb34570a4d196522de0cbba1688
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-11 08:38:23 +0900

    シェルスクリプトの整理

--- scripts/d-app.sh
+++ scripts/d-app.sh
@@ -4,16 +4,16 @@
 source .env
 
 echo $APP_CONTAINER_NAME"コンテナを操作したい"
-echo "1. linter.py 実行 (品質向上コマンドを実行します。)"
-echo "2. コンテナ内 ls 実行"
+echo "1. pyモジュールによる品質向上"
+echo "2. コンテナ内のlsコマンド 実行"
 read -p "(1,2)を選択してください。: " choice
 
 case $choice in
   1)
-    ./docker-exec.sh $APP_CONTAINER_NAME python3 linter.py
+    ./scripts/libs/docker-exec.sh $APP_CONTAINER_NAME python3 linter.py
     ;;
   2)
-    ./docker-exec.sh $APP_CONTAINER_NAME ls
+    ./scripts/libs/docker-exec.sh $APP_CONTAINER_NAME ls
     ;;
   *)
     echo "Invalid choice"
