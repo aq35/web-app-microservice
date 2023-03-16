7906ef50ae8c5f081b378f638e62131353734db2
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-17 08:31:19 +0900

    途中

--- sh/app.sh
+++ sh/app.sh
@@ -7,7 +7,8 @@ echo $APP_CONTAINER_NAME"コンテナを操作したい"
 echo "1. py品質向上ツールを実行する"
 echo "2. データベースの初期化"
 echo "3. ls 実行"
-read -p "(1,2,3)を選択してください。: " choice
+echo "4. アプリ 起動"
+read -p "(1,2,3,4)を選択してください。: " choice
 
 case $choice in
   1)
@@ -18,6 +19,9 @@ case $choice in
     ;;
   3)
     ./sh/libs/docker-exec.sh $APP_CONTAINER_NAME ls
+    ;;
+  4)
+    ./sh/libs/docker-exec.sh $APP_CONTAINER_NAME flask --app flaskr run --debug
     ;;    
   *)
     echo "Invalid choice"
