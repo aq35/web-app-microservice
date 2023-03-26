9ed738311b45d3171e1503b8a596b9849d9a832d
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-27 08:05:43 +0900

    Update db.py

--- app/flask-tutorial/flaskr/db.py
+++ app/flask-tutorial/flaskr/db.py
@@ -5,27 +5,76 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# このプログラムは、Flaskアプリケーションにおいて、データベース接続を行い、
+# データベースの初期化や接続のクローズを行うためのユーティリティ関数群を定義しています。
 
-def get_db():
-    if 'db' not in g:
-        g.db = mysql.connector.connect(
-            pool_name='my_pool',
-            pool_size=16,
-            pool_reset_session=True,
-            host=current_app.config['DB_HOST'],
-            user=current_app.config['DB_USER'],
-            password=current_app.config['DB_PASSWORD'],
-            database=current_app.config['DB_NAME'],
+
+# MySQLデータベースへの接続や切断を行うためのメソッドを提供しています。
+
+class YourDBConnector:
+    # データベースへの接続に必要な情報を受け取ります。
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.conn = None
+
+    # connectメソッドでは、mysql.connectorモジュールを使用して実際に接続を行っています。
+    def connect(self):
+        self.conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
         )
+        return self.conn
 
+    # 接続を切断するための処理を行っています。
+    def close(self):
+        if self.conn:
+            self.conn.close()
+
+    def init_db(self, conn, schema_file):
+        cursor = conn.cursor()
+        for statement in schema_file.read().decode('utf8').split(';'):
+            cursor.execute(statement)
+        cursor.close()
+        conn.commit()
+
+
+# Flaskアプリケーションのコンテキスト内で唯一のデータベース接続オブジェクトを取得するための関数
+# gオブジェクトを使用して、コンテキスト内で同じ接続オブジェクトを再利用できるようにしています。
+def connect_db():
+    if 'db' not in g:
+        g.db = get_db_connector().connect()
     return g.db
 
 
+def get_db_connector():
+    if 'db_connector' not in g:
+        g.db_connector = YourDBConnector(
+            host=current_app.config['DB_HOST'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD'],
+            database=current_app.config['DB_NAME']
+        )
+    return g.db_connector
+
+
+# connect_db関数を呼び出して、唯一のデータベース接続オブジェクトを取得するための関数
+def get_db():
+    if 'db' not in g:
+        g.db = connect_db()
+    return g.db
+
+
+# Flaskアプリケーションのコンテキストが終了した際に、データベース接続をクローズするための関数
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
-        db.close()
+        get_db_connector().close()
 
 
 @click.command('init-db')
@@ -35,13 +84,13 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 
-def init_db(app):
-    db = get_db()
-    db.init_app(app)
-    # with current_app.open_resource('schema.sql') as f:
-    #     cursor = db.cursor()
-    #     for statement in f.read().decode('utf8').split(';'):
-    #         cursor.execute(statement)
+# データベースの初期化を行うための関数
+def init_db():
+    db_connector = get_db_connector()
+    conn = db_connector.connect()
+    with current_app.open_resource('schema.sql') as f:
+        db_connector.init_db(conn, f)
+    conn.close()
 
 
 def init_app(app):
