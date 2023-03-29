26fa2c1c11890f53bc867944f8ea991d0d4f5fa2
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2023-03-30 07:49:05 +0900

    Update __init__.py

--- app/flask-tutorial/flaskr/__init__.py
+++ app/flask-tutorial/flaskr/__init__.py
@@ -11,22 +11,18 @@ mail = Mail()
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = 'my_secret_key'
-    # app.config.from_mapping(
-    #     SECRET_KEY="dev",
-    #     DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    # )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-        'pool_pre_ping': True,
-        'pool_recycle': 3600,
-    }
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql/flask-app?charset=utf8mb4'
-    app.config.from_mapping(
-        # ...
+
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:password@mysql/flask-app?charset=utf8mb4',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ENGINE_OPTIONS={
+            'pool_pre_ping': True,
+            'pool_recycle': 3600,
+        },
         DB_HOST='mysql',
         DB_USER='root',
         DB_PASSWORD='password',
-        DB_NAME='flask-app'
+        DB_NAME='flask-app',
     )
 
     # ログ設定を行う
@@ -41,12 +37,14 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
-    app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
-    app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
-    app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-    app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+    app.config.update({
+        "MAIL_SERVER": os.environ.get("MAIL_SERVER"),
+        "MAIL_PORT": os.environ.get("MAIL_PORT"),
+        "MAIL_USE_TLS": os.environ.get("MAIL_USE_TLS"),
+        "MAIL_USERNAME": os.environ.get("MAIL_USERNAME"),
+        "MAIL_PASSWORD": os.environ.get("MAIL_PASSWORD"),
+        "MAIL_DEFAULT_SENDER": os.environ.get("MAIL_DEFAULT_SENDER"),
+    })
 
     mail.init_app(app)
 
